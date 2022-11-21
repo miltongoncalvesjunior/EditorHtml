3c781cb5d87e04bb655f9bb6f664a470274c0fd4
Junior gonçalves <decimo.terceiro13@hotmail.com>
2022-11-21 15:44:38 -0300

    0.6

---
+++ Viewer.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace EditorHtml{
+    public class Viewer{
+
+        public static void Show(string text){
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor =  ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO VIZUALIZAÇÃO");
+            Console.WriteLine("-----------------");
+            Replace(text);
+            Console.WriteLine("-----------------");
+            Console.ReadKey();
+            Menu.Show();
+
+        }
+
+        public static void Replace(string text)
+        {
+
+            var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+            Console.WriteLine("");
+
+
+        }
+
+
+    }
+}
